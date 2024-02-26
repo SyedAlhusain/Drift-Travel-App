f0b6c4bd6c12d0a7d7cdad14ee1ff2994b5f4ed4
SyedAlhusain <108646522+SyedAlhusain@users.noreply.github.com>
2024-02-25 21:27:42 -0500

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -6,8 +6,8 @@ from flask_login import LoginManager
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '453685vstja3r5'
 # Connect to the database
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://pascal:uAZg04eUXKhO@ep-tiny-voice-59659868.' \
-                                        'us-east-2.aws.neon.tech/neondb?sslmode=require'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://husainsalamdar786:S9sIUkHC7XyP@ep-hidden-union-07506131.us-east-2.aws.neon.tech/Rove-Travel-App?sslmode=require'
+
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
@@ -15,3 +15,5 @@ login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
 
 from app import routes
+
+
